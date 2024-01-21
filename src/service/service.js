b2c19ed7b2e2409b8f1dd4347d4304a8c2c38a44
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-01-21 19:46:33 +0530

    content update

--- src/service/service.js
+++ src/service/service.js
@@ -1,7 +1,7 @@
 import { get, post } from './api_helpers';
 
-export const saveUser = (data) => post('http://127.0.0.1:8000/create-user', data);
-export const geAllUsers = () => get('http://127.0.0.1:8000/fetch-all-users');
+export const saveUser = (data) => post('http://127.0.0.1:8001/create-user', data);
+export const fetchAllUsers = () => get('http://127.0.0.1:8001/fetch-all-users');
 export const FetchAllAnalyzedDataByBubbleMap = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/bubble-map');
 export const FetchAllAnalyzedDataByHour = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/hour');
 export const fetchAllAnalyzedDataByDay = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/day');
--- src/views/sign-in-and-up/SignInAndUp.js
+++ src/views/sign-in-and-up/SignInAndUp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../sign-in-and-up/SignInAndUp.css';
 import googleIcon from '../../assets/icons8-google-48.png';
 import facebookIcon from '../../assets/icons8-facebook-48.png';
@@ -8,7 +8,7 @@ import { toast } from 'react-toastify';
 import { Field, Form, Formik } from 'formik';
 import * as yup from 'yup';
 import { Button } from 'reactstrap';
-import { geAllUsers, saveUser } from '../../service/service';
+import { fetchAllUsers, getAllUsers, saveUser } from '../../service/service';
 
 const SignInAndUp = () => {
   const containerRef = useRef(null);
@@ -18,14 +18,20 @@ const SignInAndUp = () => {
     navigate('/system/dashboard');
   };
 
-  const getAllUsers = async () => {
+  const getAllUsersFunc = async (values) => {
     try {
-      const response = await geAllUsers();
-      for (let resp of response.data) {
-        console.log(resp.name);
-      }
-      // toast.success("Successfully Insert");
-      // navigateToDashboard();
+      const response =  fetchAllUsers();
+      response.then((result) => {
+        for (let resp of result.data) {
+          if (resp.email === values.new_email && resp.password === values.new_password) {
+            console.log('Successfully Login');
+            navigateToDashboard();
+            return;
+          }
+        }
+      });
+
+      toast.error('Invalid email or password');
     } catch (error) {
       toast.error('Error');
     }
@@ -34,11 +40,11 @@ const SignInAndUp = () => {
   const formSubmitManually = async (values) => {
     try {
       const response = await saveUser(values);
-      console.log('values ***************************** ' + response);
-      console.log(values);
+      console.log('Response from saveUser:', response);
       toast.success('Successfully Insert');
       navigateToDashboard();
     } catch (error) {
+      console.error('Error saving user:', error);
       toast.error('Error');
     }
   };
@@ -50,9 +56,8 @@ const SignInAndUp = () => {
   });
 
   const validationSignInForm = yup.object().shape({
-    name: yup.string().required('Name is required'),
-    email: yup.string().email('Invalid email').required('Email is required'),
-    password: yup.string().required('Password is required'),
+    new_email: yup.string().email('Invalid email').required('Email is required'),
+    new_password: yup.string().required('Password is required'),
   });
 
   useEffect(() => {
@@ -78,71 +83,101 @@ const SignInAndUp = () => {
     }
   }, []);
 
-  return (<div className="container" ref={containerRef}>
-    <div className="form-container sign-up-container">
-      <Formik
-        enableReinitialize
-        initialValues={{
-          name: '', email: '', password: '',
-        }}
-        validationSchema={validationSignUpForm}
-        onSubmit={formSubmitManually}
-      >
-        {({ isSubmitting }) => (<Form className="form">
-          <h3>Create Account</h3>
-          <div className="social-container">
-            <img alt="Google Icon" className="social" src={googleIcon} />
-            <img alt="Facebook Icon" className="social" src={facebookIcon} />
-            <img alt="Apple Icon" className="social" src={appleIcon} />
+  return (
+    <div className="container" ref={containerRef}>
+      <div className="form-container sign-up-container">
+        <Formik
+          enableReinitialize
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+          }}
+          validationSchema={validationSignUpForm}
+          onSubmit={formSubmitManually}
+        >
+          {({ isSubmitting }) => (
+            <Form className="form">
+              <h3>Create Account</h3>
+              <div className="social-container">
+                <img alt="Google Icon" className="social" src={googleIcon} />
+                <img alt="Facebook Icon" className="social" src={facebookIcon} />
+                <img alt="Apple Icon" className="social" src={appleIcon} />
+              </div>
+              <span>or use your email for registration</span>
+              <Field className="input" placeholder="Name" type="text" name="name" />
+              <Field className="input" placeholder="Email" type="email" name="email" />
+              <Field className="input" placeholder="Password" type="password" name="password" />
+              <Button type="submit" disabled={isSubmitting}>
+                Sign Up
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      <div className="form-container sign-in-container">
+        <Formik
+          enableReinitialize
+          initialValues={{
+            new_email: '',
+            new_password: '',
+          }}
+          validationSchema={validationSignInForm}
+          onSubmit={getAllUsersFunc}
+        >
+          {({ isSubmitting }) => (
+            <Form className="form">
+              <h3>Sign in</h3>
+              <div className="social-container">
+                <img alt="Google Icon" className="social" src={googleIcon} />
+                <img alt="Facebook Icon" className="social" src={facebookIcon} />
+                <img alt="Apple Icon" className="social" src={appleIcon} />
+              </div>
+              <span>or use your account</span>
+              <Field
+                className="input"
+                name="new_email"
+                placeholder="Email"
+                id="new_email"
+                type="email"
+              />
+              <Field
+                className="input"
+                placeholder="Password"
+                name="new_password"
+                id="new_password"
+                type="password"
+              />
+              <a href="/forgot-password">Forgot your password?</a>
+              <Button type="submit" disabled={isSubmitting}>
+                Sign In
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h3>Welcome Back!</h3>
+            <p style={{ fontSize: '16px', fontWeight: '800' }}>
+              To keep connected with us, please login with your personal info
+            </p>
+            <button className="ghost" id="signIn">
+              Sign In
+            </button>
           </div>
-          <span>or use your email for registration</span>
-          <Field className="input" placeholder="Name" type="text" name="name" />
-          <Field className="input" placeholder="Email" type="email" name="email" />
-          <Field className="input" placeholder="Password" type="password" name="password" />
-          <Button type="submit" disabled={isSubmitting}>Sign Up</Button>
-        </Form>)}
-      </Formik>
-    </div>
-    <div className="form-container sign-in-container">
-      <Formik
-        enableReinitialize
-        initialValues={{
-          email: '', password: '',
-        }}
-        validationSchema={validationSignInForm}
-        onSubmit={getAllUsers}
-      >
-        {({ isSubmitting }) => (<Form className="form">
-          <h3>Sign in</h3>
-          <div className="social-container">
-            <img alt="Google Icon" className="social" src={googleIcon} />
-            <img alt="Facebook Icon" className="social" src={facebookIcon} />
-            <img alt="Apple Icon" className="social" src={appleIcon} />
+          <div className="overlay-panel overlay-right">
+            <h3>Hello, aboard!</h3>
+            <p style={{ fontSize: '16px', fontWeight: '800' }}>Welcome to the system.</p>
+            <button className="ghost" id="signUp">
+              Sign Up
+            </button>
           </div>
-          <span>or use your account</span>
-          <Field className="input" name="email" placeholder="Email" type="email" />
-          <Field className="input" placeholder="Password" name="password" type="password" />
-          <a href="/forgot-password">Forgot your password?</a>
-          <Button type="submit" disabled={isSubmitting}>Sign In</Button>
-        </Form>)}
-      </Formik>
-    </div>
-    <div className="overlay-container">
-      <div className="overlay">
-        <div className="overlay-panel overlay-left">
-          <h3>Welcome Back!</h3>
-          <p style={{ fontSize: '16px', fontWeight: '800' }}>To keep connected with us please login with your
-            personal info</p>
-          <button className="ghost" id="signIn">Sign In</button>
-        </div>
-        <div className="overlay-panel overlay-right">
-          <h3>Hello, aboard!</h3>
-          <p style={{ fontSize: '16px', fontWeight: '800' }}>Welcome to the system.</p>
-          <button className="ghost" id="signUp">Sign Up</button>
         </div>
       </div>
     </div>
-  </div>);
+  );
 };
 
 export default SignInAndUp;
