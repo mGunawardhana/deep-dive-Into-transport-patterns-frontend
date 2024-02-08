7ead9f0a2e11093903196b8382c4cd07b10ac4f7
mGunawardhana <mrgunawardhana27368@gmail.com>
2024-02-09 00:51:21 +0530

    content update

--- src/service/service.js
+++ src/service/service.js
@@ -17,7 +17,7 @@ export const updateUser = (data) => {
   return put(`http://127.0.0.1:8001/${data.id}`, userData);
 };
 export const fetchAllUsers = () => get('http://127.0.0.1:8001/fetch-all-users');
-export const FetchAllAnalyzedDataByBubbleMap = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/bubble-map');
-export const FetchAllAnalyzedDataByHour = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/hour');
-export const fetchAllAnalyzedDataByDay = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/day');
+export const FetchAllAnalyzedDataByBubbleMap = () => get('http://3.111.39.177/level-one/optimised/data/visualise/bubble-map');
+export const FetchAllAnalyzedDataByHour = () => get('http://3.111.39.177/level-one/optimised/data/visualise/hour');
+export const fetchAllAnalyzedDataByDay = () => get('http://3.111.39.177/level-one/optimised/data/visualise/day');
 export const fetchAllAnalyzedDataBubble = () => get('http://127.0.0.1:8000/level-one/optimised/data/visualise/bubble-map');
